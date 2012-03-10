7f7248650bee15a05b412d1424bac9096852a319
Greg Fefelov <greg.fefelov@gmail.com>
2012-03-10 23:26:47 +0400

    trivial make stuff
    Signed-off-by: Greg Fefelov <greg.fefelov@gmail.com>

---
+++ metamake.py
@@ -0,0 +1,23 @@
+import os
+
+if os.name == "nt":
+	pattern = "echo compiling %s\n d:/erl5.9/bin/erlc.exe -o ebin %s\n\n"
+	prologue = "@echo off\n"
+elif os.name == "posix":
+	pattern = "echo compiling %s\n erlc -o ebin %s\n\n"
+	prologue = "#!/bin/sh\n"
+
+files = os.listdir(".")
+mkf = open("make.bat" if os.name == "nt" else "make.sh", "w")
+mkf.write(prologue)
+
+f2 = []
+for f in files:
+	if f[-4:] == ".erl":
+		f2.append(f)
+
+print f2
+for f in f2:
+	mkf.write(pattern % (f,f))
+
+mkf.close()
