bd171ca7aa13b22484dc86b0bb93c6682b572afd
Greg Fefelov <greg.fefelov@gmail.com>
2012-03-12 21:57:20 +0400

    chunks are now actually encoded also, add_data support

--- src/records.hrl
+++ src/records.hrl
@@ -1,2 +1,2 @@
 -record(chunk_column_data, {full_column, types, metadata,
-                            block_light, sky_light, biome}).
+                            block_light, sky_light, add_data, biome}).
