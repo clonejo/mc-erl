7df11668d9f7d7d7d08ad683eb38ae997e5dcc75
Feiko Nanninga <admin@clonejo.de>
2012-03-13 17:35:45 +0100

    added server structure stuff

--- src/mc_erl_app.erl
+++ src/mc_erl_app.erl
@@ -1,5 +1,12 @@
 -module(mc_erl_app).
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) -> mc_erl_proxy:start_link().
+
+%% StartArgs = proxy | server
+%% StartArgs = [] is deprecated!
+start(_StartType, StartArgs) ->
+	case StartArgs of
+		[] -> mc_erl_proxy:start_link();
+		[proxy] -> mc_erl_proxy:start_link();
+		[server] -> mc_erl_server:start_link();
 stop(_State) -> mc_erl_proxy:stop().
---
+++ src/mc_erl_server.erl
@@ -0,0 +1,71 @@
+-module(mc_erl_proxy).
+-behaviour(gen_server).
+
+-export([start_link/0, stop/0]).
+
+-include("records.hrl").
+
+% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+start_link() ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+stop() ->
+	gen_server:cast(?MODULE, stop).
+
+
+% gen_server callbacks
+init([]) ->
+	io:format("[~s] starting~n", [?MODULE]),
+	Port = 25566,
+	{ok, Listen} = gen_tcp:listen(Port, [binary, {reuseaddr, true}, {active, false},
+	                           {packet, raw}, {nodelay, true}]),
+		spawn_link(fun() -> acceptor(Listen) end),
+		{ok, Listen}.
+
+acceptor(Listen) ->
+	case gen_tcp:accept(Listen) of
+		{ok, Socket} ->
+			gen_server:cast(?MODULE, {new_connection, Socket}),
+			acceptor(Listen);
+		{error, closed} ->
+			ok
+	end.
+
+handle_call(Message, _From, State) ->
+	case Message of
+		_ ->
+			io:format("[~s] received call: ~p~n", [?MODULE, Message]),
+			{noreply, State}
+	end.
+
+
+handle_cast({new_connection, ClientSocket}, State) ->
+	io:format("[~s] Player connecting...~n", [?MODULE]),
+	spawn(fun() ->
+		client_to_server(ClientSocket, ServerSocket, LogHandler) end),
+	{noreply, State};
+
+handle_cast(stop, State) ->
+	io:format("[~s] stopping~n", [?MODULE]),
+	{stop, normal, State};
+
+handle_cast(Message, State) ->
+	io:format("[~s] received cast: ~p~n", [?MODULE, Message]),
+	{noreply, State}.
+
+handle_info(Message, State) ->
+	case Message of
+		_ ->
+			io:format("[~s] received info: ~p~n", [?MODULE, Message]),
+			{noreply, State}
+	end.
+
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+
