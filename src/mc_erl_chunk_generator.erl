46949cd1382e7a41f13bcb86cee2d1c94eb551ef
Feiko Nanninga <admin@clonejo.de>
2012-03-14 15:38:40 +0100

    fixed chunk generator, added debug chunk manager

--- src/mc_erl_chunk_generator.erl
+++ src/mc_erl_chunk_generator.erl
@@ -1,14 +1,14 @@
--modulue(mc_erl_chunk_generator).
+-module(mc_erl_chunk_generator).
 
--export([gen_chunk/1]).
+-export([gen_column/1]).
 
 -include("records.hrl").
 
 gen_column({chunk, X, Y}) ->
-	#chunk_column_data(full_column=true,
-	                   types=[{0, binary:copy(<<7>>,256)
-	                              ++ binary:copy(<<0>>,256*15)}],
-	                   metadata=[{0, binary:copy(<<0>>,16*16*16/2)}],
-	                   block_light=[{0, binary:copy(<<255>>,16*16*16/2)}],
-	                   sky_light=[{0, binary:copy(<<255>>,16*16*16/2)}],
-	                   biome=binary:copy(<<0>>,256)).
+	#chunk_column_data{full_column=true,
+	                   types=[{0, list_to_binary([binary:copy(<<7>>,256),
+	                                             binary:copy(<<0>>,256*15)])}],
+	                   metadata=[{0, binary:copy(<<0>>,16*16*8)}],
+	                   block_light=[{0, binary:copy(<<255>>,16*16*8)}],
+	                   sky_light=[{0, binary:copy(<<255>>,16*16*8)}],
+	                   biome=binary:copy(<<0>>,256)}.
--- src/mc_erl_chunks.erl
+++ src/mc_erl_chunks.erl
@@ -1,7 +1,7 @@
 -module(mc_erl_chunks).
 -behaviour(gen_server).
 
--export([start_link/0, stop/0]).
+-export([start_link/0, stop/0, coord_to_chunk/1, chunks_in_range/2, get_chunk/1]).
 
 -include("records.hrl").
 
@@ -14,11 +14,39 @@ start_link() ->
 stop() ->
 	gen_server:cast(?MODULE, stop).
 
+%% converts coordinates to chunk coordinates
+coord_to_chunk({pos, X, _Y, Z}) ->
+	{chunk, floor(X/16), floor(Z/16)}.
+
+floor(X) when X < 0 ->
+    T = trunc(X),
+    case X - T == 0 of
+        true -> T;
+        false -> T - 1
+    end;
+floor(X) ->
+    trunc(X).
+
+%% returns a set of {chunk, X, Z} coordinates
+chunks_in_range({pos, _, _, _}=Pos, Range) ->
+	chunks_in_range(coord_to_chunk(Pos), Range);
+chunks_in_range({chunk, CX, CZ}, Range) ->
+	sets:from_list(lists:flatten(
+		[[{chunk, X, Z} || X<-lists:seq(CX-Range, CX+Range)]||
+			Z<-lists:seq(CZ-Range, CZ+Range)])).
+
+get_chunk({pos, _, _, _}=Pos) ->
+	get_chunk(coord_to_chunk(Pos));
+get_chunk({chunk, _, _}=Coord) ->
+	gen_server:call(?MODULE, {get_chunk, Coord}).
 
 % gen_server callbacks
 init([]) ->
-	
+	io:format("[~s] starting~n", [?MODULE]),
+	{ok, []}.
 
+handle_call({get_chunk, ChunkCoord}, From, State) ->
+	mc_erl_chunk_generator:gen_column(ChunkCoord);
 handle_call(Message, _From, State) ->
 	case Message of
 		_ ->
