f9772d4fb43e63cb6e7be1e64f3d374ec1d418c8
Feiko Nanninga <admin@clonejo.de>
2012-03-21 22:11:33 +0100

    split long chat messages to avoid disconnecting clients

--- src/mc_erl_chat.erl
+++ src/mc_erl_chat.erl
@@ -9,12 +9,31 @@ broadcast(Player, Message) ->
 
 broadcast(Message) ->
 	Players = mc_erl_entity_manager:get_all_players(),
-	lists:map(fun(Player) -> Player#player.player_logic ! {chat, Message} end, Players).
+	lists:map(fun(Player) -> to_player(Player, Message) end, Players).
 
 
-%% Receiver = player record or name
+%% Receiver = player record or name or pid
 to_player(Name, Message) when is_list(Name) ->
 	Receiver = mc_erl_entity_manager:get_player(Name),
 	to_player(Receiver, Message);
-to_player(Receiver, Message) ->
-	Receiver#player.player_logic ! {chat, Message}.
+	
+to_player(Receiver, Message) when is_tuple(Receiver) ->
+	to_player(Receiver#player.player_logic, Message);
+	
+to_player(Logic, Message) when is_pid(Logic) ->
+	Parts = split_message(Message),
+	lists:map(fun(Part) -> Logic ! {chat, Part} end, Parts).
+
+
+split_message(Message) ->
+	split_message(Message, []).
+
+split_message(Message, Output) ->
+	if length(Message) =< 119 ->
+		lists:reverse([Message|Output]);
+	true ->
+		{Part, Rest} = lists:split(119, Message),
+		split_message(Rest, [Part|Output])
+	end.
+
+
