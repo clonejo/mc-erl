321070f6d1d36ef19eafb0cffde38853b3f231a9
Feiko Nanninga <admin@clonejo.de>
2012-04-16 22:11:50 +0200

    Dropped the proxy application. In case you need the proxy: mc_erl_proxy:start_link()

--- src/mc_erl_app.erl
+++ src/mc_erl_app.erl
@@ -2,11 +2,6 @@
 -export([start/2, stop/1]).
 
 
-%% StartArgs = proxy | server
-%% StartArgs = [] is deprecated!
-start(_StartType, [server]) -> mc_erl_server_sup:start_link();
-start(_StartType, [proxy]) -> mc_erl_proxy:start_link().
+start(_StartType, _StartArgs) -> mc_erl_server_sup:start_link();
 
-stop(_State) -> [
-	mc_erl_proxy:stop(),
-	mc_erl_server_sup:shutdown()].
+stop(_State) -> mc_erl_server_sup:shutdown().
